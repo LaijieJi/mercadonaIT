d913eeccfc398c2db96f838e65112f0e861bc5a0
Jorge Pardo <jorgeparpar@gmail.com>
2025-04-14 12:44:11 +0200

    feat(App): print response

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,7 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Camera, ChefHat, LucideAngularModule, Sparkles } from 'lucide-angular';
+import { type ServerResponse } from './models';
+import {
+  Camera,
+  ChefHat,
+  LoaderCircle,
+  LucideAngularModule,
+  Sparkles,
+} from 'lucide-angular';
+
+const exampleData: ServerResponse = {
+  name: 'Spaghetti Carbonara',
+  recipe: [
+    'Boil spaghetti in salted water until al dente.',
+    'In a separate pan, cook pancetta until crispy.',
+    'In a bowl, whisk together eggs and cheese.',
+    'Drain spaghetti and add to pancetta pan.',
+    'Remove from heat and quickly mix in egg and cheese mixture.',
+    'Serve immediately with extra cheese and pepper.',
+  ],
+  ingredients: [
+    {
+      name: 'Spaghetti',
+      img_url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2kbsX1IbOLCT4bVs_rFueXiXrdiBMMDhbEw&s',
+      price: 1.5,
+    },
+    {
+      name: 'Pancetta',
+      img_url: 'https://m.media-amazon.com/images/I/71goicKgmSL.jpg',
+      price: 3.0,
+    },
+    {
+      name: 'Eggs',
+      img_url:
+        'https://bakerpedia.com/wp-content/uploads/2020/03/Egg_baking-ingredients-e1584136402126.jpg',
+      price: 0.2,
+    },
+    {
+      name: 'Parmesan Cheese',
+      img_url:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Parmigiano_Reggiano%2C_Italien%2C_Europ%C3%A4ische_Union.jpg/960px-Parmigiano_Reggiano%2C_Italien%2C_Europ%C3%A4ische_Union.jpg',
+      price: 2.5,
+    },
+  ],
+};
 
 @Component({
   selector: 'app-root',
@@ -13,10 +57,13 @@ export class AppComponent {
   title = 'mercadona-chef';
 
   imageSrc: string | ArrayBuffer | null = null;
+  isSubmitting = false;
+  response: ServerResponse | null = null;
 
   readonly Camera = Camera;
   readonly Sparkles = Sparkles;
   readonly ChefHat = ChefHat;
+  readonly LoaderCircle = LoaderCircle;
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
@@ -28,29 +75,36 @@ export class AppComponent {
       reader.readAsDataURL(input.files[0]);
     }
   }
-
-  onSubmit(): void {
+  async onSubmit(): Promise<void> {
     if (this.imageSrc) {
+      this.isSubmitting = true;
       const formData = new FormData();
       const blob = this.dataURLToBlob(this.imageSrc);
       formData.append('image', blob, 'uploaded-image.png');
 
-      fetch('http://localhost:8000', {
-        method: 'POST',
-        body: formData,
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Failed to upload image');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log('Image uploaded successfully:', data);
-        })
-        .catch((error) => {
-          console.error('Error uploading image:', error);
+      try {
+        const response = await fetch('http://localhost:8000', {
+          method: 'POST',
+          body: formData,
         });
+
+        if (!response.ok) {
+          throw new Error('Failed to upload image');
+        }
+
+        const data = await response.json();
+        console.log('Image uploaded successfully:');
+        console.log('Name:', data.name);
+        console.log('Recipe:', data.recipe);
+        console.log('Ingredients:', data.ingredients);
+
+        this.response = data;
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        this.response = exampleData;
+      } finally {
+        this.isSubmitting = false;
+      }
     } else {
       console.error('No image selected to upload.');
     }
